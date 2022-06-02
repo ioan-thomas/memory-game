ae2479d8738ad4d0b9cc40ef8cbc7c885d4b4a49
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-02 09:51:02 +0100

    Fixed bug where cards wouldn't load.

--- src/App.js
+++ src/App.js
@@ -24,12 +24,24 @@ function App() {
     setTurns(0)
   }
 
-  console.log(2)
+  console.log(cards)
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => {
+          return (
+          <div className='card' key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src='img/cover.png' alt="card back" />
+            </div>
+          </div>
+        )})}
+      </div>
     </div>
   );
 }
